7bba609e01c55665521c5e37e91fd44a55d7c84e
Pedro Augusto Dourado Izarias <pedro.izarias@grupoorion.com.br>
2024-10-11 14:38:03 -0300

    Adding put and delete routes for Todo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,5 +27,36 @@ namespace Todo.Controllers{
         {
             return context.Todos.FirstOrDefault(x=>x.Id == id);
         }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x=>x.Id==id);
+            if(model ==null) return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return model;
+         }
+
+         [HttpDelete("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x=>x.Id==id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return model;
+         }
     }
 }
